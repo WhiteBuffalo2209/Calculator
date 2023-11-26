8c33841f776207f53423601bb90b84f8c3eed8de
WhiteBuffalo2209 <luigi1229@yahoo.com>
2023-11-26 14:03:25 +0900

    Create function addition, subtraction, multiplication, division

---
+++ javascript/code.js
@@ -0,0 +1,15 @@
+
+//add, subtract, multiple, divide
+
+function addition(a,b){
+    return a+b;
+}
+function subtraction(a,b){
+    return a-b;
+}
+function multiplication(a,b){
+    return a*b;
+}
+function division(a,b){
+    return a/b;
+}
