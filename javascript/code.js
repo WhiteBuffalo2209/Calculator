295ce406aee92fbead5b109e5754436e13f750eb
WhiteBuffalo2209 <luigi1229@yahoo.com>
2023-11-28 08:38:13 +0900

    Create function operate, make mathematical functions return numbers instead of strings

--- javascript/code.js
+++ javascript/code.js
@@ -1,15 +1,27 @@
 
-//add, subtract, multiple, divide
+let initialVal;
+let nextVal;
+let mathOperator;
 
 function addition(a,b){
-    return a+b;
+    return +a + +b;
 }
 function subtraction(a,b){
-    return a-b;
+    return +a - +b;
 }
 function multiplication(a,b){
-    return a*b;
+    return +a * +b;
 }
 function division(a,b){
-    return a/b;
+    return +a / +b;
+}
+function operate(){ //need checking
+    initialVal = prompt('Enter inital value:');
+    mathOperator = prompt('Enter math operator:');
+    nextVal = prompt('Enter next value:');
+    let result;
+    if(mathOperator == '+'){
+        result = addition(initialVal,nextVal); //need checking
+    }
+    return result;
 }
