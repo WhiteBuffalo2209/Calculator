da1b40fa9c37c26d7af058f46329f851f4b2b4ae
WhiteBuffalo2209 <luigi1229@yahoo.com>
2023-12-21 08:28:15 +0900

    Re-construct the function operate to contain all the mathematical operation when equals button is pressed

--- javascript/code.js
+++ javascript/code.js
@@ -3,9 +3,6 @@ let storageVal = '';
 let buttonClicked;
 let buttonArray = [];
 
-
-
-
 function addition(a,b){
     return +a + +b;
 }
@@ -18,21 +15,37 @@ function multiplication(a,b){
 function division(a,b){
     return +a / +b;
 }
-function operate(){ //need checking
-    let result;
-    if(mathOperator == '+'){
-        result = addition(initialVal,nextVal);
+function operate(){
+    for(i=0;i<buttonArray.length-1;i++){   //need to make new function, too long
+        if(buttonArray[i] === '+'){         //need to add multiplication and also divison
+            if(i>=3){
+                initial = result;
+                next = buttonArray[i+1];
+                result = addition(initial,next);
+                console.log(`next ${next} initial ${initial} answer ${result}`); 
+            }
+            else{
+                initial = buttonArray[i-1];
+                next = buttonArray[i+1];
+                result = addition(initial,next);
+                console.log(`next ${next} initial ${initial} answer ${result}`); 
+            }
+        }
+        else if(buttonArray[i] === '-'){
+            if(i>=3){
+                initial = result;
+                next = buttonArray[i+1];
+                result = subtraction(initial,next);
+                console.log(`next ${next} initial ${initial} answer ${result}`); 
+            }
+            else{
+                initial = buttonArray[i-1];
+                next = buttonArray[i+1];
+                result = subtraction(initial,next);
+                console.log(`next ${next} initial ${initial} answer ${result}`); 
+            }
+        }
     }
-    else if (mathOperator == '-'){
-        result = subtraction(initialVal,nextVal);
-    }
-    else if (mathOperator == '*'){
-        result = multiplication(initialVal,nextVal);
-    }
-    else if (mathOperator == '÷'){
-        result = division(initialVal,nextVal);
-    }
-    return result;
 }
 
 function testFunction(e) {
@@ -51,42 +64,11 @@ function testFunction(e) {
         let initial;
         let next;
         let result;
-        for(i=0;i<buttonArray.length-1;i++){   //need to make new function, too long
-            if(buttonArray[i] === '+'){         //need to add multiplication and also divison
-                if(i>=3){
-                    initial = result;
-                    next = buttonArray[i+1];
-                    result = addition(initial,next);
-                    console.log(`next ${next} initial ${initial} answer ${result}`); 
-                }
-                else{
-                    initial = buttonArray[i-1];
-                    next = buttonArray[i+1];
-                    result = addition(initial,next);
-                    console.log(`next ${next} initial ${initial} answer ${result}`); 
-                }
-            }
-            else if(buttonArray[i] === '-'){
-                if(i>=3){
-                    initial = result;
-                    next = buttonArray[i+1];
-                    result = subtraction(initial,next);
-                    console.log(`next ${next} initial ${initial} answer ${result}`); 
-                }
-                else{
-                    initial = buttonArray[i-1];
-                    next = buttonArray[i+1];
-                    result = subtraction(initial,next);
-                    console.log(`next ${next} initial ${initial} answer ${result}`); 
-                }
-            }
-            
-        }
+        operate();
     }
     else{
         storageVal += buttonClicked;
     }
-    
 }
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => button.addEventListener('click',testFunction));
